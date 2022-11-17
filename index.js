b9e77c9fe712e0e7e61995f13fe455401a4e0d79
Ashraf59 <ashrafams59b@gmail.com>
2022-11-17 16:39:09 +0600

    Doctors Portal Server Created

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l1ydak8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ 
+async function run(){
+  try{
+    const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
+    console.log(appointmentOptionCollection);
+
+    app.get('/appointmentOptions', async(req, res) => {
+      const query = {}
+      const options = await appointmentOptionCollection.find(query).toArray();
+      res.send(options);
+    })
+  }
+  finally{
+
+  }
+
+}
+
+run().catch(console.log);
+
+
+
+app.get('/', async(req, res) => {
+    res.send('Doctors portal is running')
+})
+
+app.listen(port, () => console.log(`Doctore portal is running on port ${port}`))
