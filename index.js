b2c490bedc4dfda179e04568fcf8f843cbc7861d
Ashraf59 <ashrafams59b@gmail.com>
2022-11-18 13:00:23 +0600

    Appointment booked added

--- index.js
+++ index.js
@@ -23,14 +23,37 @@ async function run(){
     const bookingsCollection = client.db('doctorsPortal').collection('bookings');
 
     app.get('/appointmentOptions', async(req, res) => {
+      const date = req.query.date;
+      // console.log(date);
       const query = {}
       const options = await appointmentOptionCollection.find(query).toArray();
+      const bookingQuery = {appointmentDate: date}
+      const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+      //Code carefully D:
+      options.forEach(option => {
+        const optionBooked = alreadyBooked.filter(book => book.treatment === option.name)
+        const bookedSlots = optionBooked.map(book => book.slot)
+        const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
+        option.slots = remainingSlots;
+        console.log(date, option.name, bookedSlots);
+      })
       res.send(options);
     })
 
     app.post('/bookings', async(req, res) => {
       const booking = req.body;
       console.log(booking);
+      const query = {
+        appointmentDate: booking.appointmentDate,
+        email: booking.email,
+        treatment: booking.treatment
+      }
+
+      const alreadyBooked = await bookingsCollection.find(query).toArray();
+      if(alreadyBooked.length){
+        const message = `You have booked on ${booking.appointmentDate}`
+        return res.send({acknowledge: false, message})
+      }
       const result = await bookingsCollection.insertOne(booking);
       res.send(result)
       console.log(result);
